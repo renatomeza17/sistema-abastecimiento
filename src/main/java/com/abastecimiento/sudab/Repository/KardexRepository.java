14f954af12e624ae0eb418f259126355b991a53b
Renato Meza <254360150+renatomeza17@users.noreply.github.com>
2026-06-30 14:25:38 -0500

    Cambio kardexrepository

--- src/main/java/com/abastecimiento/sudab/Repository/KardexRepository.java
+++ src/main/java/com/abastecimiento/sudab/Repository/KardexRepository.java
@@ -13,5 +13,7 @@ public interface KardexRepository extends JpaRepository<Kardex, Long>{
 
     Optional<Kardex> findByProductoIdProducto(Long idProducto);
 
+    boolean existsByProductoIdProducto(Long idProducto);
+
 
 }
